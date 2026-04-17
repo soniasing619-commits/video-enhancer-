caf31402500573c72492160a84e2a0215c484819
soniasing619-commits <soniasing619@gmail.com>
2026-04-17 17:10:47 +0500

    Create main.py

---
+++ backend/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+import shutil, os
+from enhancer import enhance_video
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+os.makedirs("uploads", exist_ok=True)
+os.makedirs("outputs", exist_ok=True)
+
+@app.post("/upload/")
+async def upload_video(file: UploadFile = File(...)):
+    path = f"uploads/{file.filename}"
+
+    with open(path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    output = enhance_video(path)
+
+    return {"output": output}
